5099ee821dadb5758bcd2591d5e2c81d8d296bf0
rwal127 <rwal127@wgu.edu>
2021-11-17 20:59:22 +0200

    Changed API pin and Key assign method to .env

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@
  * You can place your custom package configuration in here.
  */
 return [
-    'pin'       => '88888888', // Pin of your APP, get it here https://www.thetvdb.com/dashboard/account/subscription
-    'apikey'    => 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbb', // v4 API Key https://www.thetvdb.com/dashboard/account/apikey
+    'pin'       => env('THETVDB_PIN', null), // Pin of your APP, get it here https://www.thetvdb.com/dashboard/account/subscription
+    'apikey'    => env('THETVDB_APIKEY', null), // v4 API Key https://www.thetvdb.com/dashboard/account/apikey
 ];
