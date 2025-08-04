8326d460fbf0899067b9bffb7b0278066a349cfa
rwal127 <samsgs2@gmail.com>
2025-08-04 15:26:24 +0300

    PHP 8.4 support

--- src/TvdbV4Client.php
+++ src/TvdbV4Client.php
@@ -15,6 +15,26 @@ class TvdbV4Client
     const API_URL = 'https://api4.thetvdb.com/v4/';
 
     /**
+     * @var string
+     */
+    private string $pin;
+
+    /**
+     * @var string
+     */
+    private string $apikey;
+
+    /**
+     * @var Client
+     */
+    private Client $tvdb_api;
+
+    /**
+     * @var array
+     */
+    private array $headers;
+
+    /**
      * TvdbV4Client constructor.
      */
     public function __construct()
@@ -35,13 +55,14 @@ class TvdbV4Client
 
 
     /**
-     * @param $method - Called method
-     * @param $id - ID
+     * @param string $method - Called method
+     * @param string|int $id - ID
      * @param bool $extended - use extended data or not
+     * @param array $params - Additional parameters
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function getData($method, $id, bool $extended = false, $params = [])
+    private function getData(string $method, $id, bool $extended = false, array $params = [])
     {
         // Check if extended needed
         $ext = ($extended) ? '/extended' : '';
@@ -61,11 +82,11 @@ class TvdbV4Client
 
 
     /**
-     * @param $method - Called method
+     * @param string $method - Called method
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function getTypeData($method)
+    private function getTypeData(string $method)
     {
         $data = $this->tvdb_api->get(self::API_URL.$method.'/types', [
             'headers' => $this->headers
@@ -78,11 +99,11 @@ class TvdbV4Client
 
 
     /**
-     * @param $method - Called method
+     * @param string $method - Called method
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function getStatusData($method)
+    private function getStatusData(string $method)
     {
         $data = $this->tvdb_api->get(self::API_URL.$method.'/statuses', [
             'headers' => $this->headers
@@ -94,13 +115,13 @@ class TvdbV4Client
     }
 
     /**
-     * @param $method - Called method
-     * @param $id - ID
-     * @param bool $extended - use extended data or not
+     * @param string $method - Called method
+     * @param string|int $id - ID
+     * @param string $lang - Language code
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function getTranslationsData($method, $id, $lang)
+    private function getTranslationsData(string $method, $id, string $lang)
     {
         $data = $this->tvdb_api->get(self::API_URL.$method.'/'.$id.'/translations/'.$lang, [
             'headers' => $this->headers
@@ -114,22 +135,23 @@ class TvdbV4Client
 
 
     /**
-     * @param $id - TheTVDB ID
+     * @param string|int $id - TheTVDB ID
+     * @param array $params - Additional parameters
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getSeries($id, $params = [])
+    public function getSeries($id, array $params = [])
     {
         return $this->getData('series', $id, true, $params);
     }
 
     /**
-     * @param $id - TheTVDB ID
+     * @param string|int $id - TheTVDB ID
      * @param string $lang
      * @return \stdClass
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getEpisodesFull ($id, string $lang = 'eng')
+    public function getEpisodesFull($id, string $lang = 'eng')
     {
         $data = new \stdClass();
         $data->extended = $this->getEpisodes($id, [
@@ -142,7 +164,7 @@ class TvdbV4Client
     }
 
     /**
-     * @param $id - ID of episode on TheTVDB
+     * @param string|int $id - ID of episode on TheTVDB
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -153,7 +175,7 @@ class TvdbV4Client
 
 
     /**
-     * @param $id - ID of Season on TheTVDB
+     * @param string|int $id - ID of Season on TheTVDB
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -163,7 +185,7 @@ class TvdbV4Client
     }
 
     /**
-     * @param $id - ID of Movie on TheTVDB
+     * @param string|int $id - ID of Movie on TheTVDB
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -173,7 +195,7 @@ class TvdbV4Client
     }
 
     /**
-     * @param $id - ID of Artwork on TheTVDB
+     * @param string|int $id - ID of Artwork on TheTVDB
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -183,7 +205,7 @@ class TvdbV4Client
     }
 
     /**
-     * @param $id - ID of Artwork on TheTVDB
+     * @param string|int $id - ID of Artwork on TheTVDB
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -193,7 +215,7 @@ class TvdbV4Client
     }
 
     /**
-     * @param $id - ID of People on TheTVDB
+     * @param string|int $id - ID of People on TheTVDB
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -204,7 +226,7 @@ class TvdbV4Client
 
 
     /**
-     * @param $id - ID of Characters on TheTVDB
+     * @param string|int $id - ID of Characters on TheTVDB
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -217,12 +239,14 @@ class TvdbV4Client
      * Search on TheTVDB
      *
      * @param string $query - additional search query param string
-     * @param string $type - restrict results to entity type movie|series|person|company
-     * @param number $year - restrict results to a year for movie|series
-     * @param number $offset - offset results
-     * @param number $limit - limit results
+     * @param string|null $type - restrict results to entity type movie|series|person|company
+     * @param int|null $year - restrict results to a year for movie|series
+     * @param int|null $offset - offset results
+     * @param int|null $limit - limit results
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function search($query, $type = null, $year = null, $offset = null, $limit = null)
+    public function search(string $query, ?string $type = null, ?int $year = null, ?int $offset = null, ?int $limit = null)
     {
         // Check if extended params needed
         $type = ($type) ? '&type='.$type : '';
@@ -250,7 +274,7 @@ class TvdbV4Client
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getSeriesByName($name)
+    public function getSeriesByName(string $name)
     {
         $found = $this->search($name, 'series', null, null, 1);
         $data = $this->getSeries($found[0]->tvdb_id);
@@ -363,12 +387,12 @@ class TvdbV4Client
     /**
      * Get Movie Translations records.
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getMoviesTranslations($id, $lang)
+    public function getMoviesTranslations($id, string $lang)
     {
         return $this->getTranslationsData('movies', $id, $lang = 'eng');
     }
@@ -376,12 +400,12 @@ class TvdbV4Client
     /**
      * Get Series Translations records.
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getSeriesTranslations($id, $lang)
+    public function getSeriesTranslations($id, string $lang)
     {
         return $this->getTranslationsData('series', $id, $lang = 'eng');
     }
@@ -390,12 +414,12 @@ class TvdbV4Client
     /**
      * Get people translation record
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getPeopleTranslations($id, $lang)
+    public function getPeopleTranslations($id, string $lang)
     {
         return $this->getTranslationsData('people', $id, $lang = 'eng');
     }
@@ -404,12 +428,12 @@ class TvdbV4Client
     /**
      * Get seasons translation record
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getSeasonsTranslations($id, $lang)
+    public function getSeasonsTranslations($id, string $lang)
     {
         return $this->getTranslationsData('seasons', $id, $lang = 'eng');
     }
@@ -417,12 +441,12 @@ class TvdbV4Client
     /**
      * Get episodes translation record
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getEpisodesTranslations($id, $lang)
+    public function getEpisodesTranslations($id, string $lang)
     {
         return $this->getTranslationsData('episodes', $id, $lang = 'eng');
     }
@@ -430,12 +454,12 @@ class TvdbV4Client
     /**
      * Get series extended record + translation record
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
-     * @return mixed
+     * @return \stdClass
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getSeriesFull ($id, string $lang = 'eng')
+    public function getSeriesFull($id, string $lang = 'eng')
     {
         $data = new \stdClass();
         $data->extended = $this->getSeries($id);
@@ -447,12 +471,12 @@ class TvdbV4Client
     /**
      * Get seasons extended record + translation record
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
-     * @return mixed
+     * @return \stdClass
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getSeasonsFull ($id, string $lang = 'eng')
+    public function getSeasonsFull($id, string $lang = 'eng')
     {
         $data = new \stdClass();
         $data->extended = $this->getSeasons($id);
@@ -465,12 +489,12 @@ class TvdbV4Client
     /**
      * Get people extended record + translation record
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
-     * @return mixed
+     * @return \stdClass
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getPeopleFull ($id, string $lang = 'eng')
+    public function getPeopleFull($id, string $lang = 'eng')
     {
         $data = new \stdClass();
         $data->extended = $this->getPeople($id);
@@ -482,12 +506,12 @@ class TvdbV4Client
     /**
      * Get movie extended record + translation record
      *
-     * @param string $id - ID on TheTVDB
+     * @param string|int $id - ID on TheTVDB
      * @param string $lang - Translation language, eng default
-     * @return mixed
+     * @return \stdClass
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getMoviesFull ($id, string $lang = 'eng')
+    public function getMoviesFull($id, string $lang = 'eng')
     {
         $data = new \stdClass();
         $data->extended = $this->getMovies($id);
